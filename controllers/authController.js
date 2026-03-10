d4a0b3b7ccf7fa7af12c94a836301cce6c1fa06c
ayoubhanine <ayoubhanine2000@gmail.com>
2026-03-10 14:44:51 +0000

    add login and register of controllers+routes+middlewares

---
+++ controllers/authController.js
@@ -0,0 +1,64 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const register = async (req,res) => {
+
+  try {
+
+    const {name,email,password} = req.body;
+
+    const userExists = await User.findOne({email});
+
+    if(userExists){
+      return res.status(400).json({message:"Email déjà utilisé"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+
+    res.status(201).json({message:"Utilisateur créé"});
+
+  } catch(error){
+    res.status(500).json({error:"Erreur serveur"});
+  }
+
+};
+export const login = async (req,res)=>{
+
+  try{
+
+    const {email,password} = req.body;
+
+    const user = await User.findOne({email});
+
+    if(!user){
+      return res.status(400).json({message:"Email invalide"});
+    }
+
+    const match = await bcrypt.compare(password,user.password);
+
+    if(!match){
+      return res.status(400).json({message:"Mot de passe incorrect"});
+    }
+
+    const token = jwt.sign(
+      {id:user._id, role:user.role},
+      process.env.JWT_SECRET,
+      {expiresIn:"1d"}
+    );
+
+    res.json({token});
+
+  }catch(error){
+    res.status(500).json({error:"Erreur serveur"});
+  }
+
+};

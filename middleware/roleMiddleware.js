6d30700f32659f866c1bb7438428ea53359b1bc7
ayoubhanine <ayoubhanine2000@gmail.com>
2026-03-13 14:27:52 +0000

    ajouter un midlleware pour autoriser juste un seul admmin

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,3 +1,5 @@
+import User from "../models/User.js";
+
 export const isAdmin = (req,res,next)=>{
 
   if(req.user.role !== "admin"){
@@ -7,3 +9,19 @@ export const isAdmin = (req,res,next)=>{
   next();
 
 };
+export const ValidateRole=async(req,res,next)=>{
+  try{
+    const {role}=req.body;
+    if(role==="admin"){
+        const existingAdmin= await User.findOne({role:"admin"});
+        if(existingAdmin){
+          return res.status(403).json({message:"admin already exists,we can accept one"})
+        }
+    }
+    next()
+  }
+  catch(err){
+console.log("Error on validating role.", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,8 +2,9 @@ import express from "express";
 import { register,login } from "../controllers/authController.js";
 import { registerValidation } from "../validators/userValidator.js";
 import { validate } from "../middleware/validationMiddleware.js";
+import { ValidateRole } from "../middleware/roleMiddleware.js";
 const router=express.Router()
-router.post("/register",registerValidation,validate,register);
+router.post("/register",registerValidation,ValidateRole,validate,register);
 router.post("/login",login);
 
 export default router;

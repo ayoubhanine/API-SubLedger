5121d3b0673ae6f716c7d153f590fb1292968ab7
ayoubhanine <ayoubhanine2000@gmail.com>
2026-03-13 10:22:15 +0000

    apply express-validtor fonctionalites on the subscriptionRoutes file

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -5,12 +5,14 @@ import { createSubscription,
     updateSubscription,
     deleteSubscription } from "../controllers/subscriptionController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import { subscriptionValidation } from "../validators/subscriptionValidator.js";
+import { validate } from "../middleware/validationMiddleware.js";
 
 const router=express.Router()
-router.post("/",protect,createSubscription);
+router.post("/",subscriptionValidation,validate,protect,createSubscription);
 router.get("/",protect,getSubscriptions);
 router.get("/:id",protect,getSubscriptionById);
-router.put("/:id",protect,updateSubscription);
+router.put("/:id",subscriptionValidation,validate,protect,updateSubscription);
 router.delete("/:id",protect,deleteSubscription);
 
 export default router;
